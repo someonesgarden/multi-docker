d078dfb15c88651b7aff30ac4fa74673686497f8
Daisuke Nishimura <d@someonesgarden.org>
2019-05-07 18:25:16 +0900

    changed travis yml2

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
